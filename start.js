46b1833db163c26a0778e50de9034f0d1e6eb52c
LUCIA FARDELLA <lfardella@uc.cl>
2023-12-02 10:49:08 -0300

    arreglito

--- start.js
+++ start.js
@@ -1,24 +1,14 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 async function deploy() {
+  await exec('npm install');
 
-  const { stdout: output1 } = await exec("npm install");
-  console.log("Installing dependencies...");
-  console.log(output1);
+  await exec('nvm install 16.0.0');
 
-  const { stdout: output2 } = await exec("nvm install 16.0.0");
-  console.log("Installing dependencies...");
-  console.log(output2);
+  await exec('nvm use 16.0.0');
 
-  const { stdout: output3 } = await exec("nvm use 16.0.0");
-  console.log("Installing dependencies...");
-  console.log(output3);
-
-  const { stdout: output4 } = await exec("yarn install");
-  console.log("Installing dependencies...");
-  console.log(output4);
-
+  await exec('yarn install');
 }
 
 deploy();
